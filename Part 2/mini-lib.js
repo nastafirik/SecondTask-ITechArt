e99e49077c8a1d63d96359f1eea316eb1703ce53
Anastasiya <nastafirik@gmail.com>
2016-07-27 18:10:35 +0300

    add work with positions

--- Part 2/mini-lib.js
+++ Part 2/mini-lib.js
@@ -1,4 +1,4 @@
-'use strict';
+var mimiLibrary=(function() {
 
         var toString = Object.prototype.toString;
 
@@ -35,15 +35,51 @@
 		}
 
 
-  	function first(arr) {
+  		function first(arr) {
             return arr[0];
         }
 
 
         function last(arr) {
-	    var n=arr.length;
-	    var lastIndex=n-1;
-	    return arr[lastIndex];
+			var n=arr.length;
+			var lastIndex=n-1;
+			return arr[lastIndex];
         }
 
 
+		function take(arr, number) {
+			var newArr = [];
+			var n = arr.length;
+			if (number > n) number = n;
+			for (var i = 0; i < number; i++) {
+				newArr.push(arr[i]);
+			}
+			return newArr;
+        }
+
+    	function skip(arr, number) {
+			var newArr = [];
+			var n = arr.length;
+			for (var i = number; i < n; i++) {
+				newArr.push(arr[i]);
+			}
+			return newArr;
+    	}
+
+
+		return {
+				isBoolean: isBoolean,
+				isArray: isArray,
+				isDate: isDate,
+				isNumber: isNumber,
+				isString: isString,
+				isFunction: isFunction,
+				isUndefined: isUndefined,
+				isNull: isNull,
+				first: arrFirst,
+				last: arrLast,
+				take: take,
+				skip: skip,
+				asChain: asChain
+			}
+})();
