a8008354f8bb3f3b9af2626726ca38d5e5b6a269
Anastasiya <nastafirik@gmail.com>
2016-07-27 17:43:22 +0300

    add work with array

--- Part 2/mini-lib.js
+++ Part 2/mini-lib.js
@@ -1,50 +1,49 @@
 'use strict';
-var workWithTypes = {
+
         var toString = Object.prototype.toString;
 
         function isArray(obj) {
             return toString.call(obj) === "[object Array]";
-        },
+        }
 
         function isBoolean(obj) {
                 toString.call(obj) === "[object Boolean]";
-        },
+        }
 
         function isDate(obj) {
             return toString.call(obj) === "[object Date]";
-        },
+        }
 
         function isNumber(obj) {
             return toString.call(obj) === "[object Number]";
-        },
+        }
 
         function isString(obj) {
             return toString.call(obj) === "[object String]";
-        },
+        }
 
         function isFunction(obj) {
             return toString.call(obj) === "[object Function]";
-        },
+        }
 
         function isUndefined(obj) {
             return obj === undefined;
-        },
+        }
 
         function isNull(obj) {
             return obj === null;
 		}
-};
 
-var workWithArrays={
-	
-		function first(arr) {
+
+  		function first(arr) {
             return arr[0];
-        },
-			
-	 	function last(arr) {
-            var leng=arr.length;
-			var lastElem=n-1;
-			return arr[lastElem];
         }
-	
-}
+
+
+        function last(arr) {
+			var n=arr.length;
+			var lastIndex=n-1;
+			return arr[lastIndex];
+        }
+
+
